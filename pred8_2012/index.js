459325553687235da7cd0fa8ba45091f4f5bcbc8
IvanaBurgen <ivana.bunjan@yahoo.com>
2023-01-19 19:58:24 +0100

    predavnje 8

---
+++ pred8_2012/index.js
@@ -0,0 +1,20 @@
+/*arrow funkcija*/
+
+const stvoriElement = (event) => {
+  event.preventDefault();
+  let podaci = dohvatiPodatkeNovogElementa();
+  document.body.appendChild(stvoriNode(podaci));
+};
+
+const dohvatiPodatkeNovogElementa = () => {
+  return {
+    elem: document.getElementById("elem").value,
+    elemSadrzaj: document.getElementById("elemSadrzaj").value,
+  };
+};
+
+const stvoriNode = (params) => {
+  let noviElement = document.createElement(params.elem);
+  noviElement.innerText = params.elemSadrzaj;
+  return noviElement;
+};
